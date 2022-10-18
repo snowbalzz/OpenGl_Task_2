8974596d6490ee7b11bdf60e0377752d3ab380f4
snowbalzz <alek0007@hz.nl>
2022-10-19 01:16:10 +0200

    Initial Commit

---
+++ Computer_grafik_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Computer_grafik_1
+//
+//  Created by Daniils Aleksasins on 19/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
